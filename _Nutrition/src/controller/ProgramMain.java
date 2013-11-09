8ce89390ecc404124d55e10356540f6a8d61f959
Joel Schmid <me@joelschmid.ch>
2013-11-09 18:08:51 +0100

    Test Commit

---
+++ _Nutrition/src/controller/ProgramMain.java
@@ -0,0 +1,21 @@
+/*
+ * HES-SO Valais, Sierre
+ * _Nutrition , controller
+ * ProgramMain.java
+ * joel.schmid
+ * 9 Nov 2013 18:08:25
+ *
+ */
+package controller;
+
+/**
+ * @author joel.schmid
+ *
+ */
+public class ProgramMain {
+	
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+
+}
