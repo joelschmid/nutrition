cce98a17bb27bada3ad81941f08651db9ed4e975
Joel Schmid <me@joelschmid.ch>
2013-11-09 17:35:36 +0100

    Initial Commit

---
+++ _Nutrition/src/testing/Testing.java
@@ -0,0 +1,25 @@
+/*
+ * HES-SO Valais, Sierre
+ * _Nutrition , testing
+ * Testing.java
+ * joel.schmid
+ * 9 Nov 2013 17:35:09
+ *
+ */
+package testing;
+
+/**
+ * @author joel.schmid
+ *
+ */
+public class Testing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing");
+	}
+
+}
