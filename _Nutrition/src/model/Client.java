9977853b0f220965220d430b4d1e46d937d50f5f
Joel Schmid <me@joelschmid.ch>
2013-11-09 18:14:35 +0100

    Add Client

---
+++ _Nutrition/src/model/Client.java
@@ -0,0 +1,17 @@
+/*
+ * HES-SO Valais, Sierre
+ * _Nutrition , model
+ * Client.java
+ * joel.schmid
+ * 9 Nov 2013 18:14:24
+ *
+ */
+package model;
+
+/**
+ * @author joel.schmid
+ *
+ */
+public class Client {
+
+}
